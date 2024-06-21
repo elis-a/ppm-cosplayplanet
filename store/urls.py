2e8e1ed1b01d7dd96484f4b455d1eb982d79183d
Elisa Farri <165005778+elis-a@users.noreply.github.com>
2024-06-21 10:06:34 +0200

    First commit

---
+++ store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
+    path('search/', views.product_search, name="product_search"),
+
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+
+    path('update_item/', views.updateItem, name="update_item"),
+    path('process_order/', views.processOrder, name="process_order"),
+]
